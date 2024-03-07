9dce8c6db371d44d8a331cc7a8489fbee48dca54
Arithorn <peet@gouws.com>
2024-03-07 08:33:31 +0000

    fix: models and url

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ const config = {
 };
 
 const gptOptions = [
-  { key: "GPT4-Turbo", text: "GPT4 Turbo", value: "gpt-4-1106-preview" },
-  { key: "GPT3.5-Turbo", text: "GPT3.5 Turbo", value: "gpt-3.5-turbo-1106" },
+  { key: "GPT4-Turbo", text: "GPT4 Turbo", value: "gpt-4-turbo-preview" },
+  { key: "GPT3.5-Turbo", text: "GPT3.5 Turbo", value: "gpt-3.5-turbo" },
   { key: "GPT4", text: "GPT 4", value: "gpt-4" },
 ];
 
